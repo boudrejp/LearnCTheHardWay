3afd775e42c1c2901858c1d602bf3a45e948d5c7
John Boudreaux <john.boudreaux.93@gmail.com>
2018-12-11 17:15:49 +0000

    messages about attempts for string

--- ex3/ex3.c
+++ ex3/ex3.c
@@ -2,14 +2,18 @@
 #include <stdio.h>
 
 /* define main procedure for program */
-int main()
+int main(int argc, char **argv[])
 {
-	int age = 10;
+	int age = 11;
 	int height = 72;
+	char print_string[25] = "Something to print";
 
 	printf("I am %d years old.\n", age);
-	printf("I am %d inches tall.\n", height);
+	// note that strings are kinda counterintuitive
+	 printf("I am %d inches tall.\n", height);
 
+	// test printing something with character
+	// printf("%c", print_string);
 	return 0;
 }
 
