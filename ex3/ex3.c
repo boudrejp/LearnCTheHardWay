9c2664f0fc9e61877fdd53ebbf49294544120078
John Boudreaux <john.boudreaux.93@gmail.com>
2018-12-11 17:04:51 +0000

    working file according to ex

---
+++ ex3/ex3.c
@@ -0,0 +1,16 @@
+/* this will include our standard io elements for printing */
+#include <stdio.h>
+
+/* define main procedure for program */
+int main()
+{
+	int age = 10;
+	int height = 72;
+
+	printf("I am %d years old.\n", age);
+	printf("I am %d inches tall.\n", height);
+
+	return 0;
+}
+
+
